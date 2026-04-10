f990bff9eaf75c1c257179648359bac93e651a8b
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-04-10 18:22:38 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SpanishTermsOfEndearment from './SpanishTermsOfEndearment';
+
+export default function App() {
+  return (
+    <SpanishTermsOfEndearment />
+  );
+}
